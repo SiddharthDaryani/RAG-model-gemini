687ece1f76431938e44e2f92ac88e07774990805
SiddahrthDaryani <siddharthdaryani49@gmail.com>
2025-09-15 12:46:39 +0530

    updated init_setup.sh

--- init_setup.sh
+++ init_setup.sh
@@ -1,10 +1,10 @@
 echo [$(date)]: "START"
 echo [$(date)]: "creating env with python version 3.8"
-conda create --prefix ./RAGenv python==3.8 -y
+conda create --prefix ./RAGenv python==3.10 -y
 echo [$(date)]: "activating the environment"
 source activate ./RAGenv
 echo [$(date)]: "installing the dev requirements"
 pip install -r requirements.txt
-echo [$(date)]: "NOW RUNNIG app.py"
+echo [$(date)]: "NOW RUNNIG at localhost"
 uvicorn app:app --reload
 echo [$(date)]: "ALL COMMANDS COMPLETED!!!"
